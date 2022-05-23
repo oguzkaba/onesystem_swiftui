8ee707d43106c0992392966a27814d8223a5ad9f
oguzkaba <oguz-kaba@hotmail.com>
2022-05-24 00:31:53 +0300

    c1

--- onesystem_swiftui/ContentView.swift
+++ onesystem_swiftui/ContentView.swift
@@ -9,8 +9,90 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            AppLogo()
+            
+            CustomTextField()
+            
+            CustomSecureTextField()
+            
+            CustomButton(bgColor: .accentColor,label: "Login")
+            CustomButton(bgColor: .primary,label: "Exit")
+        }
+    }
+}
+
+struct AppLogo : View {
+    var body: some View {
+        Image("app_logo")
+            .resizable()
+            .scaledToFit()
+            .padding(.all,40)
+    }
+}
+
+struct CustomTextField : View {
+    @State var username : String=""
+    let tfBackColor : Color = Color(red: 239/255, green: 243/255, blue: 244/255)
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "person")
+            TextField("UserName", text: $username)
+        }
+        .frame(width: UIScreen.main.bounds.size.width*0.85, height: 30)
+        .padding()
+        .overlay(RoundedRectangle(cornerRadius:10).stroke().foregroundColor(.gray))
+        .background(tfBackColor)
+    }
+}
+
+struct CustomSecureTextField : View {
+    @State var username : String=""
+    let tfBackColor : Color = Color(red: 239/255, green: 243/255, blue: 244/255)
+    
+    var body: some View {
+        HStack {
+            Image(systemName: "lock")
+            SecureField("Password", text: $username)
+        }
+        .frame(width: UIScreen.main.bounds.size.width*0.85, height: 30)
+        .padding()
+        .overlay(RoundedRectangle(cornerRadius:10).stroke().foregroundColor(.gray))
+        .background(tfBackColor)
+        .padding(.bottom,40)
+    }
+}
+
+struct CustomButton : View {
+    let bgColor : Color
+    let label : String
+    
+    var body: some View {
+        Button(action: {
+            print("Button Tap..!")
+        }){
+            HStack{
+                Text(label)
+                    .foregroundColor(.white)
+                    .fontWeight(.black)
+                Image("cart")
+            }
+        }
+        .frame(width: UIScreen.main.bounds.size.width*0.85, height: 30)
+        .padding()
+        .background(bgColor)
+        .cornerRadius(10)
+        .onTapGesture {
+            hideKeyboard()
+        }
+    }
+}
+
+extension View{
+    func hideKeyboard(){
+        let resign=#selector(UIResponder.resignFirstResponder)
+        UIApplication.shared.sendAction(resign, to: nil, from: nil, for: nil)
     }
 }
 
